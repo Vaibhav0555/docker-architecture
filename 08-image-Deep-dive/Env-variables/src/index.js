b219e26db49c144e1e60cf703e002b2d4d589521
Vaibhav Singh <vaibhavsingh@Vaibhavs-MacBook-Air.local>
2026-08-08 19:23:57 +0530

    08-section one more lecture

--- 08-image-Deep-dive/Env-variables/src/index.js
+++ 08-image-Deep-dive/Env-variables/src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.send("Hello from express app!");
+  res.send(`<h1>Welcome to ${process.env.APP_NAME}</h1>`);
 });
 
 app.listen(port, () => {
