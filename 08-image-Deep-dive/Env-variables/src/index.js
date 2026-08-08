c6a8d2382db81a8ff65cc8a015f5a8060752ba33
Vaibhav Singh <vaibhavsingh@Vaibhavs-MacBook-Air.local>
2026-08-08 14:10:03 +0530

    08-sections added

---
+++ 08-image-Deep-dive/Env-variables/src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello from express app!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
