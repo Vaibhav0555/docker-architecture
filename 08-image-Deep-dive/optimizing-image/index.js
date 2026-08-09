1a48409758b33744dae1afa703f6732632feec3c
Vaibhav0555 <singhvaibhav841@gmail.com>
2026-08-09 15:30:29 +0000

    volume section started

--- 08-image-Deep-dive/optimizing-image/index.js
+++ 08-image-Deep-dive/optimizing-image/index.js
@@ -1,1 +1,11 @@
-console.log("Optimizing Docker Images");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    return res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
