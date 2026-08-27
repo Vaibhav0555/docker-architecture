a79cdfb06ec8b065f5ef132b9c1c1eafa3b0cfeb
Vaibhav Singh <vaibhavsingh@Vaibhavs-MacBook-Air.local>
2026-08-27 09:46:28 +0530

    project completed

--- 12-docker-project/backend/src/routes/store.js
+++ 12-docker-project/backend/src/routes/store.js
@@ -1,18 +1,92 @@
 const express = require("express");
-
+const { KeyValue } = require("../models/keyValue");
 const keyValueRouter = express.Router();
 
-keyValueRouter.post("/", (req, res) => {
+keyValueRouter.post("/", async (req, res) => {
+  const { key, value } = req.body;
+
+  if (!key || !value) {
+    return res.status(400).json({ error: "Both Key and Value are required" });
+  }
+
+  try {
+    const existingKey = await KeyValue.findOne({ key });
+
+    if (existingKey) {
+      return res.status(400).json({ error: "Key already exists" });
+    }
+
+    const keyValue = new KeyValue({ key, value });
+    await keyValue.save();
+
+    return res
+      .status(201)
+      .json({ message: "Key-value pair stored successfully" });
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server error" });
+  }
   return res.send("creating key-value pair");
 });
-keyValueRouter.get("/:key", (req, res) => {
+
+keyValueRouter.get("/:key", async (req, res) => {
+  const { key } = req.params;
+  try {
+    const keyValue = await KeyValue.findOne({ key });
+
+    if (!keyValue) {
+      return res.status(400).json({ error: "Key not found" });
+    }
+
+    return res.status(200).json({ key: keyValue.key, value: keyValue.value });
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server error" });
+  }
   return res.send("getting key-value pair");
 });
-keyValueRouter.put("/:key", (req, res) => {
-  return res.send("updating key-value pair");
+
+keyValueRouter.put("/:key", async (req, res) => {
+  const { key } = req.params;
+  const { value } = req.body;
+
+  if (!value) {
+    return res.status(400).json({ error: '"value" is required' });
+  }
+  try {
+    const keyValue = await KeyValue.findOneAndUpdate(
+      { key },
+      { value },
+      { new: true },
+    );
+
+    if (!keyValue) {
+      return res.status(400).json({ error: "Key not found" });
+    }
+
+    return res.status(200).json({
+      message: "Key-value pair updated sucessfully",
+      key: keyValue.key,
+      value: keyValue.value,
+    });
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server error" });
+  }
 });
-keyValueRouter.delete("/:key", (req, res) => {
-  return res.send("deleting key-value pair");
+
+keyValueRouter.delete("/:key", async (req, res) => {
+  const { key } = req.params;
+
+  try {
+    const keyValue = await KeyValue.findOneAndDelete({ key });
+
+    if (!keyValue) {
+      return res.status(400).json({ error: "Key not found" });
+    }
+    return res
+      .status(200)
+      .json({ message: "Key-value pair deleted sucessfully" });
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server error" });
+  }
 });
 
 module.exports = {
